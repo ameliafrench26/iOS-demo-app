bd2883a1ca6b88f262799157c9abd77f371d2a48
BT_C09_08 <mobileappdev@bths.edu>
2026-04-27 09:23:39 -0400

    added sampleview

---
+++ GitOne/SampleView.swift
@@ -0,0 +1,19 @@
+//
+//  SampleView.swift
+//  GitOne
+//
+//  Created by Student on 4/27/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Look at this sample view")
+    }
+}
+
+#Preview {
+    SampleView()
+}

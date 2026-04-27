8ed719eb3eaff00422cbe9c8b692fa789a5099ca
BT_C09_08 <mobileappdev@bths.edu>
2026-04-27 09:24:06 -0400

    added text

--- GitOne/ContentView.swift
+++ GitOne/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, bklyn tech!")
+            Text("Hello again!")
             
         }
         .padding()

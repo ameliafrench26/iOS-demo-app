3deabbf677c7b4bedcaf68ae910bbc87cfd2d728
BT_C09_08 <mobileappdev@bths.edu>
2026-04-27 09:16:04 -0400

    Initial Commit

---
+++ GitOne/GitOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitOneApp.swift
+//  GitOne
+//
+//  Created by Student on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct GitOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

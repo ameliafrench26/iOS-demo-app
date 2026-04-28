e75a0099865179c3a31a415b63aaa94ced90b2a7
BT_C09_08 <mobileappdev@bths.edu>
2026-04-28 09:06:45 -0400

    asdfasdfasdfasdfasfdF

--- GitOne/ContentView.swift
+++ GitOne/ContentView.swift
@@ -22,6 +22,7 @@ struct ContentView: View {
             Text("No web dev today")
             Text("No web dev today")
             Text("No web dev today")
+            Text("No web dev today")
             
         }
         .padding()
